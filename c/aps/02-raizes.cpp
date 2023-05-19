20090a392a01c8fab10e0fec2738f6bfbb1841f6
João Pedro Yoshizaki <Yoshizaki02@hotmail.com>
2023-05-18 21:12:04 -0300

    update ex 02

--- c/aps/02-raizes.cpp
+++ c/aps/02-raizes.cpp
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include <math.h>
+
 int main() {
-    float a, b, c;
-    float delta, x1, x2;
-    printf("Digite o coeficiente a: ");
-    scanf("%f", &a);
-    printf("Digite o coeficiente b: ");
-    scanf("%f", &b);
-    printf("Digite o coeficiente c: ");
-    scanf("%f", &c);
+    float a, b, c, delta, x1, x2;
+    printf("Digite os coeficientes a, b e c da equacao: ");
+    scanf("%f %f %f", &a, &b, &c);
+    // Verificar se a é diferente de zero
     if (a == 0) {
-        printf("Coeficiente 'a' deve ser diferente de zero.\n");
+        printf("Coeficiente 'a' nao pode ser zero.\n");
         return 0;
     }
+    // Calcular o delta
     delta = (b * b) - (4 * a * c);
+    // Verificar o valor do delta
     if (delta < 0) {
-        x1 = 0;
-        x2 = 0;
+        printf("Nao existem raizes reais.\n");
+        x1 = x2 = 0;
     } else if (delta == 0) {
-        x1 = -b / (2 * a);
-        x2 = x1;
+        printf("Existe apenas uma raiz real.\n");
+        x1 = x2 = -b / (2 * a);
     } else {
+        printf("Existem duas raizes reais.\n");
         x1 = (-b + sqrt(delta)) / (2 * a);
         x2 = (-b - sqrt(delta)) / (2 * a);
     }
-    printf("Raiz 1 (x1): %.2f\n", x1);
-    printf("Raiz 2 (x2): %.2f\n", x2);
+    // Imprimir as raizes
+    printf("x1 = %.2f\n", x1);
+    printf("x2 = %.2f\n", x2);
     return 0;
 }
